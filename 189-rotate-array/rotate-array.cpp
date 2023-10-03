d3747b98f2dc2e78e04ad172504edd4f7543fb52
Mohammad Anash <132294162+Anash001@users.noreply.github.com>
2023-10-03 19:54:52 +0530

    Time: 25 ms (32.98%) | Memory: 25.3 MB (56.50%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    void rotate(vector<int>& nums, int k) {
-    vector<int>ans;
-    int n=nums.size();
-    k=k%nums.size();
-   
+
+    void swap(int s ,int e, vector<int>& nums){
      
-    
-        reverse(nums.begin(),nums.end());
-    
+        while(s<e){
+            int temp =nums[s];
+            nums[s]=nums[e];
+            nums[e]=temp;
+            s++;
+            e--;
 
+        }
+    }
+    void rotate(vector<int>& nums, int k) {
     
-    
-    
-        reverse(nums.begin(),nums.begin()+k);
-
-   
-        reverse(nums.begin()+k,nums.end());
+    int n=nums.size();
+    swap(0,n-1,nums);
+       k=k%nums.size();
+    swap(0,k-1,nums);
+    swap(k,n-1,nums); 
     
 
     }
